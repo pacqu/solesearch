83d6c9cf65944785f4067aaaffe8dae13dfd4cf0
Justin Pacquing <pacqqu@gmail.com>
2015-11-24 18:53:29 -0500

    Getting URLs and getting HTML of page as a string works

--- util.py
+++ util.py
@@ -1,32 +1,34 @@
 import google, requests, bs4, urllib2, json
 
-'''
-Returns List of Result URLs from Google Query 
-'''
-def getQueries(query):
+
+def getPages(query):
+    '''
+    Finds the Resulting Pages from and inputted query
+    Parameter: String Query to find results for
+    Returns: List of URLs of Resulting Pages
+    '''
     res = google.search(query,num=10,start=0,stop=10)
     results = []
-    l = dir(res)
-    return l
-'''
-    for r in res:
-        results.append(r)
+    appending = res.next()
+    for i in range(10): 
+        results.append(res.next())
     return results
-'''
 
 
-'''
-Returns Contents of Page as a String
-'''
+
 def getPageString(u):
+    '''
+    Gets Page Contents as a String
+    Paramater: String of URL of Page
+    Returns: String of all HTML in Page
+    '''
     url = urllib2.urlopen(u)
-#print url
-    page = url.read().decode('ascii')
-#print page
-    soup = bs4.BeautifulSoup(page,"html.parser")
-    raw = soup.get_text(page)
-    return raw
-    
+    page = url.read()
+    #print page
+    soup = bs4.BeautifulSoup(page,'html.parser')
+    #raw = soup.get_text(page)
+    return soup.prettify()
+   
 #print  getQueries("hi")
 #print getPageString(r[0])
 
@@ -58,3 +60,5 @@ def findName(checkList):
                 result.append(value)
     return result
 
+r = getPages("pi")
+print getPageString(r[0])
