e17e7e678900909aab5b182cb1ed3031bcfd5746
JeffreyZou13 <jeffreyzou13@gmail.com>
2015-11-24 16:24:04 -0500

    tried to work with regular expressions

--- util.py
+++ util.py
@@ -1,4 +1,4 @@
-import google, requests, beautifulsoup, urllib2, json
+import google, requests, beautifulsoup, urllib2, json, re
 
 def getData():
     key= "AIzaSyBPILwQR9q7OBgW_NB2krK-Fc1PLNLJR0g"
@@ -9,5 +9,19 @@ def getData():
     r = json.loads(result)
     return r
 
-
+def findName(checkList):
+    '''Find the query using regular expression and google
+    Parameters: list of pages to look through
+    Return: Results of the search in a list form
+    '''
+    #This is supposed to look for a name
+    #legit does not work 
+    result = []
+    regExp = ""
+    for i in checkList:
+        match = re.search(regExp, i)
+        if match:
+            for value in match.group():
+                result.append(value)
+    return result
 
