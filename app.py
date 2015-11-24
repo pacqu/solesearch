3973deac2077335d080b5413ed27c233cd9521b7
Derrick Lui <derricklui1@gmail.com>
2015-11-24 12:25:20 -0500

    updated app.py

--- app.py
+++ app.py
@@ -2,9 +2,16 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods = ["GET"])
 def index():
-    return render_template("index.html")
+    return render_template("search.html")
+
+@app.route("/", methods = ["POST"])
+def index2():
+    query1 = request.form['query1']
+    query2 = request.form['query2']
+    query3 = request.form['query3']
+    util.
 
 if __name__ == "__main__":
     app.debug = True
