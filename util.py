7e44f2982e086cb42e959fd86515e9f1a24649dc
JeffreyZou13 <jeffreyzou13@gmail.com>
2015-11-29 14:55:42 -0500

    did the stopwords

--- util.py
+++ util.py
@@ -1,4 +1,4 @@
-import google, bs4, urllib2, 
+import google, bs4, urllib2,
 from stop_words import get_stop_words
 
 
@@ -11,7 +11,7 @@ def getPages(query):
     res = google.search(query,num=10,start=0,stop=10)
     results = []
     appending = res.next()
-    for i in range(10): 
+    for i in range(10):
         results.append(res.next())
     return results
 
@@ -40,12 +40,22 @@ def getPStrings(query):
     for u in urllist:
         PStrings.append(u)
     return PStrings
-    
+
 def removeStopWords(page):
     '''
     Returns Page's HTML Contents with stop words removed
     Parameter: String page that hold HTML of a page
     Returns: String of Page with Stop Words removed
+    stopwords = []
+    with open("stopwords.txt","r") as f:
+        for line in f:
+            stopwords.append(line)
+    words = page.split(" ")
+    for word in words:
+        if word in stopwords:
+            stopwords.remove(word)
+    newPage = words.join(" ")
+    return newPage
     '''
 
 r = getPages("pi")
