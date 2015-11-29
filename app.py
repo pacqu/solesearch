a84c70a6eef5905dbc636aed58b5bf870035c9b4
Derrick Lui <derricklui1@gmail.com>
2015-11-29 18:19:29 -0500

    updated app.py and some templates

--- app.py
+++ app.py
@@ -1,22 +1,32 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/", methods = ["GET"])
 def index():
-    return render_template("search.html")
+    return render_template("index.html")
 
 @app.route("/", methods = ["POST"])
 def index2():
-    query1 = request.form['query1']
-    query2 = request.form['query2']
-    query3 = request.form['query3']
-    #util.
-    return 0
+    r = ""
+    query1 = request.form['querywho']
+    query2 = request.form['querywhen']
+    query3 = request.form['querywhere']
+    if not query1 or not query2 or not query3:
+        errors = "Please fill one of the fields."
+        return render_template("index.html",errors = errors)
+    else:
+        if query1:
+            r = query1
+        elif query2:
+            r = query2
+        elif query3:
+            r = query3
+    return render_template("result.html",result = r)
 
-@app.route("/about")
+@app.route("/result")
 def about():
-    return render_template("about.html")
+    return render_template("result.html")
 
 if __name__ == "__main__":
     app.debug = True
